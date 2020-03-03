7feabffab517ac29e5a2c59f6e73651ce7fb8a3b
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-03 13:56:00 +0100

    Create vehicle model

---
+++ models/vehicle.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const vehicleSchema = new Schema({
+  type: {type: String, required: true, enum: ["bike", "motorcycle", "car", "scooter", "electric scooter"]},
+  ownerId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+  latitude: {type: String},
+  longitude: {type: String},
+  available: {type: Boolean}
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+
+module.exports = Vehicle;
