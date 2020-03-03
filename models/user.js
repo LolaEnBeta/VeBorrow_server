2719ae336c2b952c90c856d21a321588e41fd9f9
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-03 13:55:42 +0100

    Create user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  phoneNumber: {type: Number, required: true, unique: true},
+  password: {type: String, required: true},
+  owner: {type: Boolean,default: false},
+  vehicles: [{type: Schema.Types.ObjectId, ref: "Vehicle"}],
+  rentals: [{type: Schema.Types.ObjectId, ref: "Rental"}]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
