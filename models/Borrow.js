ec0446e32d5aed8e7816fad320339396226fde9a
LolaEnBeta <lolitaenbeta@gmail.com>
2020-03-04 12:03:20 +0100

    Refactor Borrow model

--- models/Borrow.js
+++ models/Borrow.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const borrowSchema = new Schema({
-  ownerId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-  renterId: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-  vehicleId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vehicle"}],
+  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  renterId: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
+  vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle"},
   message: {type: String}
 }, {
   timestamps: {
